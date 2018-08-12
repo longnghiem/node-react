76b1757cc25c8d08a52ea74dc74bb88fa3353acf
Long Nghiem <longsuomi@gmail.com>
2018-08-13 00:06:00 +0300

    minor editing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter, Route, NavLink} from 'react-router-dom'
 
 const Landing = () => <h2> Landing </h2>
+const Dashboard = () => <h2> Dashboard </h2>
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" component= {Landing} />
+          <Route exact path="/" component= {Landing} />
+          <Route path="/surveys" component= {Dashboard} />
         </div>
       </BrowserRouter>
     </div>
